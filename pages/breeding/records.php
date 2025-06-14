4201282f5404de20674b352eb35be7125d3f6fcb
Chemwa <creatorjabatar@gmail.com>
2025-06-14 20:25:50 +0300

    modified code

--- pages/breeding/records.php
+++ pages/breeding/records.php
@@ -3,8 +3,8 @@ include 'c:\xampp\htdocs\rabbit_farm\includes\config.php';
 $mysqli =  mysqli_connect($server,$user,$pass,$db_name);
 if (!$mysqli) {
     echo "Connection error". mysqli_connect_error();
-}else{ 
-    $sql = "select buck_ear_tag,doe_ear_tag,breeding_date,expected_kindling_date from breeding_records";
+}
+    $sql = "select id,buck_ear_tag,doe_ear_tag,breeding_date,expected_kindling_date,actual_kindling_date,litter_size from breeding_records";
     $result = mysqli_query($mysqli,$sql);
     $records = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
@@ -38,6 +38,8 @@ if (!$mysqli) {
           <th>Doe</th>
           <th>Breeding Date</th>
           <th>Expected Kindling Date</th>
+          <th>Actual Kindling Date</th>
+          <th>Litter Size</th>
         </tr>
       </thead>
       <tbody>
@@ -47,8 +49,31 @@ if (!$mysqli) {
             <td><?= $records[$i]['doe_ear_tag']; ?></td>
             <td><?= $records[$i]['breeding_date']; ?></td>
             <td><?= $records[$i]['expected_kindling_date']; ?></td>
+            <form action="records.php" method="post">
+            <td><input type="date" name="date" id="date" placeholder="Enter Date"></td>
+            <td><input type="number" name="litter_size" id="litter_size" placeholder="Enter Litter Size"></td>
+            <td><input type="submit" name="submit" id="submit" value="Save Changes"></td>          
+            <td><input type="number" name="id" id="id" value="<?=$records[$i]['id']?>" hidden></td> 
+            <?php
+if(isset($_POST['submit'])){
+  $id = htmlspecialchars((string)$_POST['id']);
+  $date = htmlspecialchars((string)$_POST['date']);
+  $litter = htmlspecialchars((string)$_POST['litter_size']);
+  $sql = "
+update breeding_records set actual_kindling_date = '$date', litter_size = '$litter' where id = '$id'
+  ";
+  $save = mysqli_query( $mysqli,$sql);
+  if (!$save) {
+    echo "error: ". mysqli_connect_error();
+}
+
+      } }
+
+
+?>
+
+            </form>
             </tr>
-            <?php } }?>
       </tbody>
     </table>
   </div>
--- pages/finance/transactions.php
+++ pages/finance/transactions.php
@@ -1,7 +1,19 @@
 <?php
-
-
-?>
+$db_name = "rabbit_farm";
+$server = "localhost";
+$user = "chemwa";
+$pass = "At._.420";
+$conn = mysqli_connect($server,$user,$pass,$db_name);
+if(!$conn){
+  echo "Connection Error: " . mysqli_connect_error();
+}
+$sql = "select transaction_date,type,category,sum(amount) as total,description,ear_tag from financial_transactions
+group by transaction_date,type,category,description,ear_tag
+order by sum(amount)
+;";
+$query = mysqli_query($conn,$sql);
+$result = mysqli_fetch_all( $query );
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,7 +34,19 @@
   </li>
 </ul>
   </div>
-
+  <div class="container-fluid">
+        <?php for($i= 0;$i<count($result);$i++){ ?>
+          <div>
+            <br><br>
+            <b>Date: </b><?=$result[$i][0];?> <br>
+            <b>Type:</b> <?=$result[$i][1];?><br>
+            <b>Category: </b><?=$result[$i][2];?><br>
+            <b>Total:</b> <?=$result[$i][3];?><br>
+            <b>Description:</b> <?=$result[$i][4];?><br>
+            <b>Ear Tag: </b><?=$result[$i][5];?><br><br>
+          </div>
+          
+        <?php }  ?>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
